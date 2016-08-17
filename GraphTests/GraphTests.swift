3da460a463c61643afc9646c1ba880ad3fc65523
Paul <pauljohanneskraft@users.noreply.github.com>
2016-08-17 13:50:30 +0200

    new testing environment generalized for Graph-protocol

--- GraphTests/GraphTests.swift
+++ GraphTests/GraphTests.swift
@@ -10,28 +10,31 @@ import XCTest
 import Algorithms_and_Data_structures
 
 class GraphTests: XCTestCase {
-    
-    func testGraph() {
-        var asymmGraph = Graph_Hashing<Vertex_Hashing>(rule: {
-            start, end in
-            if start.hashValue == end.hashValue - 1 { return 1 } else { return nil }
-        })
-        
-        for i in 0..<20 { asymmGraph.insert(Vertex_Hashing(value: i)) }
-        
-        print(asymmGraph)
-        
-        /*
-         asymmGraph.rule = {
-         start, end in
-         if (start.hashValue % 3) >= (end.hashValue % 5) { return 1 } else { return nil } }
-         print(asymmGraph)
-         */
-        
-        let s = asymmGraph.vertices[0]!
-        let e = asymmGraph.vertices[9]!
-        let path = asymmGraph.djikstra(start: s, end: e)
-        
-        print(path)
-    }
+	
+	func testGraph_Hashing() {
+		graphTest(graph: Graph_Hashing())
+	}
+	
+	func testGraph_Matrix() {
+		graphTest(graph: Graph_Matrix())
+	}
+	
+	func graphTest<G : Graph>(graph: G) {
+		var graph = graph
+		
+		for i in 0..<20 {
+			graph[i, i + 1] = 1
+			graph[i + 1, i] = 1
+		}
+		
+		graph[3,7] = 1
+		graph[7,3] = 1
+		
+		print(graph)
+		
+		let path = graph.djikstra(start: 0, end: 9)
+		
+		XCTAssert(path == [0, 1, 2, 3, 7, 8, 9])
+	}
+	
 }
