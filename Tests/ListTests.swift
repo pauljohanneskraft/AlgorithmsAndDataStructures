0148e1658a0d02e965775ff6a5149206ec40c953
Paul <pauljohanneskraft@users.noreply.github.com>
2017-03-18 16:30:18 +0100

    removed unnecessary empty lines in ListTests

--- Tests/ListTests.swift
+++ Tests/ListTests.swift
@@ -45,9 +45,7 @@ class ListTests: XCTestCase {
 				if let o = e as? ListError	{ XCTAssert(o == ListError.IndexOutOfRange) }
 				else						{ XCTAssert(false) }
 			}
-			
-			// print(ll.description)
-			
+            
 			XCTAssert(ll.count == arr.count)
 			XCTAssert(ll.array == arr)
 			
@@ -60,34 +58,24 @@ class ListTests: XCTestCase {
 			
 			var llRev = [Int]()
 			
-			for i in arr.indices.reversed() {
-				// print("will remove \(i):", arr[i])
-				llRev.append(try! ll.remove(at: i))
-			}
+			for i in arr.indices.reversed() { llRev.append(try! ll.remove(at: i)) }
 			
 			XCTAssert(ll.count == 0)
 			
-			// print(sllRev)
 			let arrRev : [Int] = arr.reversed()
 			XCTAssert(llRev == arrRev)
 			
 			for i in arrRev.indices { ll[i] = arrRev[i] }
-			
-			// print(sll.array)
-			XCTAssert(ll.array == arrRev)
+            
+            XCTAssert(ll.array == arrRev)
 			
 			var arr2 = [Int]()
-			
-			for _ in arrRev.indices {
-				arr2.append(ll.popBack()!)
-			}
+			for _ in arrRev.indices { arr2.append(ll.popBack()!) }
 			
 			XCTAssert(arr2 == arr)
 			XCTAssert(ll.count == 0)
 			
-			for e in arr {
-				ll.pushBack(e)
-			}
+			for e in arr { ll.pushBack(e) }
 			
 			XCTAssert(ll.array == arr, "\(ll.array) != \(arr)")
 		}
@@ -97,9 +85,7 @@ class ListTests: XCTestCase {
 			XCTAssert(l.description == "[1 -> 2 -> 3]")
 		} else if L.self == DoublyLinkedList<Int>.self {
 			XCTAssert(l.description == "[1 <-> 2 <-> 3]")
-		} else {
-			XCTAssert(false, "add this type")
-		}
+		} else { XCTAssert(false, "add this type") }
 	}
 	
     func testLazyLists() {
